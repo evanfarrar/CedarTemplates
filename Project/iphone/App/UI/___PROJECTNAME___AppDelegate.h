488fccc8e0154fbb2c2a86e0820b0fd8bc274083
Adam Milligan <amilligan@pivotallabs.com>
2010-12-02 13:00:20 -0800

    iOS project template creates production target

---
+++ Project/iphone/App/UI/___PROJECTNAME___AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  ___PROJECTNAME___AppDelegate.h
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ___PROJECTNAME___AppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
